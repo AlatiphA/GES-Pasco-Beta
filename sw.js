1fe0493c2672e647b13bfed72af016603cf4a5de
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-19 11:30:09 +0000

    Bump app version from 3.1.8 to 3.1.9

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v3.1.8";
+const APP_VERSION  = "v3.1.9";
 const CACHE_NAME   = `alatipha-ges-pasco-${APP_VERSION}`;
 
 /* ====================
