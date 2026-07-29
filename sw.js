7c6d6fd09d99cd75f6dc1055b6bb3d02b47df92f
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-29 06:12:42 +0000

    Update app version to v3.4.0

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v3.3.1";
+const APP_VERSION  = "v3.4.0";
 const CACHE_NAME   = `alatipha-ges-pasco-${APP_VERSION}`;
 
 /* ====================
